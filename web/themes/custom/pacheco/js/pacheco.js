ad5e45d2275a06acad1d923e2e9ea4cd3eaa8a38
cvillamil <cvillamil@dayscript.com>
2020-06-08 18:22:56 -0500

    update website

--- web/themes/custom/pacheco/js/pacheco.js
+++ web/themes/custom/pacheco/js/pacheco.js
@@ -8,9 +8,14 @@
   /**
    * Use this behavior as a template for custom Javascript.
    */
-  Drupal.behaviors.exampleBehavior = {
+  Drupal.behaviors.pacheco = {
     attach: function (context, settings) {
       //alert("I'm alive!");
+      $('#menu-hamburger .hamburger', context).once('pacheco').on('click',function(e){
+      	$(this).toggleClass('is-active').closest('body').toggleClass('overflow-hidden');
+      	$('#menu-hamburger').toggleClass('closed-right')
+      	$('#full-page-menu').toggleClass('hide')
+      });
     }
   };
 
