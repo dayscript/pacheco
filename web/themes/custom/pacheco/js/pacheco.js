d684b74f3cc91844af666156418dc8404590fd5d
cvillamil <cvillamil@dayscript.com>
2020-07-23 00:09:38 -0500

    js update

--- web/themes/custom/pacheco/js/pacheco.js
+++ web/themes/custom/pacheco/js/pacheco.js
@@ -20,23 +20,23 @@
       *GALERIAS PROYESTOS;*
       *********************/
       //FOTOS
-      $('#lightgallery-start-fotos, .field-name-field-galeria img', context).once('pacheco').on('click touchstart',function(e){
-        e.preventDefault();
+      $('#lightgallery-start-fotos, .field-name-field-galeria img', context).once('pacheco').on('click',function(e){
+        //e.preventDefault();
         $(".gallery-lightgallery .lightgallery-wrapper:nth-child(2) a > img").first().trigger("click");
       });
       //ESQUEMAS
-      $('#lightgallery-start-esquemas, .field-name-field-galeria-esquemas img', context).once('pacheco').on('click touchstart',function(e){
-        e.preventDefault();
+      $('#lightgallery-start-esquemas, .field-name-field-galeria-esquemas img', context).once('pacheco').on('click',function(e){
+        //e.preventDefault();
         $(".gallery-lightgallery .lightgallery-wrapper:nth-child(3) a > img").first().trigger("click");
       });
       //PLANOS
-      $('#lightgallery-start-planos, .field-name-field-galeria-planos img', context).once('pacheco').on('click touchstart',function(e){
-        e.preventDefault();
+      $('#lightgallery-start-planos, .field-name-field-galeria-planos img', context).once('pacheco').on('click',function(e){
+        //e.preventDefault();
         $(".gallery-lightgallery .lightgallery-wrapper:nth-child(4) a > img").first().trigger("click");
       });
       //REFERENTES
-      $('#lightgallery-start-referentes, .field-name-field-galeria-referentes img', context).once('pacheco').on('click touchstart',function(e){
-        e.preventDefault();
+      $('#lightgallery-start-referentes, .field-name-field-galeria-referentes img', context).once('pacheco').on('click',function(e){
+        //e.preventDefault();
         $(".gallery-lightgallery .lightgallery-wrapper:nth-child(5) a > img").first().trigger("click");
       });
       /************************
